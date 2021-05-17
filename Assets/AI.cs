29c030a17cb3842c8bac484581a4a50fc3a0079b
LuaMartins <luamartins070@gmail.com>
2021-05-17 20:45:45 -0300

    Comentado
    comentado

--- Assets/AI.cs
+++ Assets/AI.cs
@@ -7,53 +7,54 @@ using Panda;
 
 public class AI : MonoBehaviour
 {
+    // declarações
     public Transform player;
     public Transform bulletSpawn;
     public Slider healthBar;
     public GameObject bulletPrefab;
 
-    NavMeshAgent agent;
+    NavMeshAgent agent; // nav mesh do agente
     public Vector3 destination; // The movement destination.
     public Vector3 target;      // The position to aim to.
-    float health = 100.0f;
-    float rotSpeed = 5.0f;
+    float health = 100.0f; // vida do agente
+    float rotSpeed = 5.0f; // rotação dele
 
-    float visibleRange = 80.0f;
-    float shotRange = 40.0f;
+    float visibleRange = 80.0f; // visibilidade do destino dele
+    float shotRange = 40.0f; // força do tiro
 
     void Start()
     {
-        agent = this.GetComponent<NavMeshAgent>();
+        agent = this.GetComponent<NavMeshAgent>(); // chamando o navmesh
         agent.stoppingDistance = shotRange - 5; //for a little buffer
         InvokeRepeating("UpdateHealth", 5, 0.5f);
     }
 
     void Update()
     {
-        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position);
-        healthBar.value = (int)health;
-        healthBar.transform.position = healthBarPos + new Vector3(0, 60, 0);
+        Vector3 healthBarPos = Camera.main.WorldToScreenPoint(this.transform.position); // colocando a barra de vida no game
+        healthBar.value = (int)health; // valor da barra de vida
+        healthBar.transform.position = healthBarPos + new Vector3(0, 60, 0); // posicionamento dela
     }
 
     void UpdateHealth()
     {
-        if (health < 100)
+        if (health < 100) // vida total
             health++;
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "bullet")
+        if (col.gameObject.tag == "bullet") // tiro do personagem
         {
-            health -= 10;
+            health -= 10; // tira vida 
         }
     }
 
     [Task] // task de referencia para puglin
     public void PickRandomDestination() // metodo para movimentação de um ponto para o outro
     {
-        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100));
-        agent.SetDestination(dest);
+        Vector3 dest = new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)); // movimentação do agente
+        agent.SetDestination(dest); // destino do agente
         Task.current.Succeed();
     }
 
@@ -61,8 +62,8 @@ public class AI : MonoBehaviour
     public void MoveToDestination() // timer para medição de onde ele chega
     {
         if (Task.isInspected)
-            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time);
-        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+            Task.current.debugInfo = string.Format("t={0:0.00}", Time.time); // timer de medição
+        if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending) // distancia do agente virtual
         {
             Task.current.Succeed();
         }
--- Assets/Drive.cs
+++ Assets/Drive.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Drive : MonoBehaviour {
-
+    // declarações
 	float speed = 20.0F;
     float rotationSpeed = 120.0F;
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
 
-    void Update() {
-        float translation = Input.GetAxis("Vertical") * speed;
-        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
-        translation *= Time.deltaTime;
-        rotation *= Time.deltaTime;
-        transform.Translate(0, 0, translation);
-        transform.Rotate(0, rotation, 0);
+    void Update() { // iniciando o update
+        float translation = Input.GetAxis("Vertical") * speed; // velocidade 
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed; // rotação para outro lado, no caso horizontal
+        translation *= Time.deltaTime; // tempo de reação para translação
+        rotation *= Time.deltaTime; // tempo de reação para rotação
+        transform.Translate(0, 0, translation); // posição da translação
+        transform.Rotate(0, rotation, 0); // posição da rotação
 
-        if(Input.GetKeyDown("space"))
+        if(Input.GetKeyDown("space")) // se apertar espaçõ
         {
-            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
-            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000);
+            GameObject bullet = GameObject.Instantiate(bulletPrefab, bulletSpawn.transform.position, bulletSpawn.transform.rotation); // instancia projeteis de bala
+            bullet.GetComponent<Rigidbody>().AddForce(bullet.transform.forward*2000); // força e rigidBody das balas
         }
     }
 }
